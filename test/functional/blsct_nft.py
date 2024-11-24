062aedbaed5ebfd80ba5a7ba09bd50df81383605
alex v <alex@encrypt-s.com>
2024-11-24 21:28:39 +0100

    fix linter

--- test/functional/blsct_nft.py
+++ test/functional/blsct_nft.py
@@ -20,9 +20,9 @@ class NavioBlsctNftTest(BitcoinTestFramework):
 
     def run_test(self):
         self.log.info("Creating wallet1 with BLSCT")
-        
+
         # Create a new wallet
-        
+
         #self.init_wallet(node=0, blsct=True)
         self.nodes[0].createwallet(wallet_name="wallet1", blsct=True)
         self.nodes[1].createwallet(wallet_name="wallet1", blsct=True)
@@ -30,30 +30,30 @@ class NavioBlsctNftTest(BitcoinTestFramework):
         wallet_info_2 = self.nodes[1].get_wallet_rpc("wallet1")
 
         self.log.info("Loading wallet1")
-        
+
         # Ensure wallet is loaded
         wallets = self.nodes[0].listwallets()
         assert "wallet1" in wallets, "wallet1 was not loaded successfully"
 
         self.log.info("Generating BLSCT address")
-        
+
         # Generate a BLSCT address
         blsct_address = wallet_info.getnewaddress(label="", address_type="blsct")
         blsct_address_2 = wallet_info_2.getnewaddress(label="", address_type="blsct")
 
         self.log.info(f"BLSCT address NODE 1: {blsct_address}")
         self.log.info(f"BLSCT address NODE 2: {blsct_address_2}")
-        
+
         # Generate blocks and fund the BLSCT address
         self.log.info("Generating 101 blocks to the BLSCT address")
         block_hashes = self.generatetoblsctaddress(self.nodes[0], 101, blsct_address)
 
         self.log.info(f"Generated blocks: {len(block_hashes)}")
-        
+
         # Check the balance of the wallet
         balance = wallet_info.getbalance()
         self.log.info(f"Balance in wallet1: {balance}")
-        
+
         assert_equal(len(block_hashes), 101)
         assert balance > 0, "Balance should be greater than zero after mining"
 
@@ -86,9 +86,9 @@ class NavioBlsctNftTest(BitcoinTestFramework):
         nft_balance = self.nodes[0].getnftbalance(token['tokenId'])
         nft_balance_2 = self.nodes[1].getnftbalance(token['tokenId'])
 
-        self.log.info(f"Balance in NDOE 1: {nft_balance}")
+        self.log.info(f"Balance in NODE 1: {nft_balance}")
         self.log.info(f"Balance in NODE 2: {nft_balance_2}")
-        
+
         assert nft_balance == {'1': {'id': 'null'}}, "incorrect nft balance in node 1"
         assert nft_balance_2 == {}, "incorrect nft balance in node 2"
 
@@ -100,9 +100,9 @@ class NavioBlsctNftTest(BitcoinTestFramework):
         nft_balance = self.nodes[0].getnftbalance(token['tokenId'])
         nft_balance_2 = self.nodes[1].getnftbalance(token['tokenId'])
 
-        self.log.info(f"Balance in NDOE 1: {nft_balance}")
+        self.log.info(f"Balance in NODE 1: {nft_balance}")
         self.log.info(f"Balance in NODE 2: {nft_balance_2}")
-        
+
         assert nft_balance_2 == {'1': {'id': 'null'}}, "incorrect nft balance in node 2"
         assert nft_balance == {}, "incorrect nft balance in node"
 
--- test/functional/blsct_token.py
+++ test/functional/blsct_token.py
@@ -20,9 +20,9 @@ class NavioBlsctTokenTest(BitcoinTestFramework):
 
     def run_test(self):
         self.log.info("Creating wallet1 with BLSCT")
-        
+
         # Create a new wallet
-        
+
         #self.init_wallet(node=0, blsct=True)
         self.nodes[0].createwallet(wallet_name="wallet1", blsct=True)
         self.nodes[1].createwallet(wallet_name="wallet1", blsct=True)
@@ -30,30 +30,30 @@ class NavioBlsctTokenTest(BitcoinTestFramework):
         wallet_info_2 = self.nodes[1].get_wallet_rpc("wallet1")
 
         self.log.info("Loading wallet1")
-        
+
         # Ensure wallet is loaded
         wallets = self.nodes[0].listwallets()
         assert "wallet1" in wallets, "wallet1 was not loaded successfully"
 
         self.log.info("Generating BLSCT address")
-        
+
         # Generate a BLSCT address
         blsct_address = wallet_info.getnewaddress(label="", address_type="blsct")
         blsct_address_2 = wallet_info_2.getnewaddress(label="", address_type="blsct")
 
         self.log.info(f"BLSCT address NODE 1: {blsct_address}")
         self.log.info(f"BLSCT address NODE 2: {blsct_address_2}")
-        
+
         # Generate blocks and fund the BLSCT address
         self.log.info("Generating 101 blocks to the BLSCT address")
         block_hashes = self.generatetoblsctaddress(self.nodes[0], 101, blsct_address)
 
         self.log.info(f"Generated blocks: {len(block_hashes)}")
-        
+
         # Check the balance of the wallet
         balance = wallet_info.getbalance()
         self.log.info(f"Balance in wallet1: {balance}")
-        
+
         assert_equal(len(block_hashes), 101)
         assert balance > 0, "Balance should be greater than zero after mining"
 
@@ -86,9 +86,9 @@ class NavioBlsctTokenTest(BitcoinTestFramework):
         token_balance = self.nodes[0].gettokenbalance(token['tokenId'])
         token_balance_2 = self.nodes[1].gettokenbalance(token['tokenId'])
 
-        self.log.info(f"Balance in NDOE 1: {token_balance}")
+        self.log.info(f"Balance in NODE 1: {token_balance}")
         self.log.info(f"Balance in NODE 2: {token_balance_2}")
-        
+
         assert token_balance == 1, "incorrect token balance in node 1"
         assert token_balance_2 == 0, "incorrect token balance in node 2"
 
@@ -103,7 +103,7 @@ class NavioBlsctTokenTest(BitcoinTestFramework):
         assert token_balance == 0.5, "incorrect token balance in node 1"
         assert token_balance_2 == 0.5, "incorrect token balance in node 2"
 
-        self.log.info(f"Balance in NDOE 1: {token_balance}")
+        self.log.info(f"Balance in NODE 1: {token_balance}")
         self.log.info(f"Balance in NODE 2: {token_balance_2}")
 
 
