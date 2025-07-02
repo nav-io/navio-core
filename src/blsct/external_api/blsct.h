0e6b3b7fb90203803745d3e79e7966c09fd98a29
gogoex <110195520+gogoex@users.noreply.github.com>
2025-07-02 13:48:20 +0900

    drop ctx in/out getters

--- src/blsct/external_api/blsct.h
+++ src/blsct/external_api/blsct.h
@@ -444,33 +444,12 @@ const BlsctOutPoint* get_tx_in_out_point(const BlsctTxIn* tx_in);
 bool get_tx_in_staked_commitment(const BlsctTxIn* tx_in);
 bool get_tx_in_rbf(const BlsctTxIn* tx_in);
 
-// ctx in (c++)
+// ctx in
 const BlsctScript* get_ctx_in_script_sig(const CTxIn* ctx_in);
 uint32_t get_ctx_in_sequence(const CTxIn* ctx_in);
 const BlsctScript* get_ctx_in_script_witness(const CTxIn* ctx_in);
 const BlsctCtxId* get_ctx_in_prev_out_hash(const CTxIn* ctx_in);
 uint32_t get_ctx_in_prev_out_n(const CTxIn* ctx_in);
-
-// ctx in (c)
-const BlsctScript* get_ctx_in_script_sig_c(
-    const uint8_t* ser_ctx_in,
-    const size_t ser_ctx_in_size
-);
-
-uint32_t get_ctx_in_sequence_c(
-    const uint8_t* ser_ctx_in,
-    const size_t ser_ctx_in_size
-);
-
-const BlsctScript* get_ctx_in_script_witness_c(
-    const uint8_t* ser_ctx_in,
-    const size_t ser_ctx_in_size
-);
-
-uint32_t get_ctx_in_prev_out_n_c(
-    const uint8_t* ser_ctx_in,
-    const size_t ser_ctx_in_size
-);
 
 // tx out
 
@@ -504,57 +483,6 @@ const BlsctPoint* get_ctx_out_ephemeral_key(const CTxOut* ctx_out);
 const BlsctPoint* get_ctx_out_blinding_key(const CTxOut* ctx_out);
 const BlsctRetVal* get_ctx_out_range_proof(const CTxOut* ctx_out);
 uint16_t get_ctx_out_view_tag(const CTxOut* ctx_out);
-
-// ctx out (c)
-uint64_t get_ctx_out_value_c(
-    const uint8_t* ser_ctx_out,
-    const size_t ser_ctx_out_size
-);
-
-const BlsctScript* get_ctx_out_script_pub_key_c(
-    const uint8_t* ser_ctx_out,
-    const size_t ser_ctx_out_size
-);
-
-const BlsctScript* get_ctx_out_script_pubkey_c(
-    const uint8_t* ser_ctx_out,
-    const size_t ser_ctx_out_size
-);
-
-const BlsctPoint* get_ctx_out_spending_key_c(
-    const uint8_t* ser_ctx_out,
-    const size_t ser_ctx_out_size
-);
-
-const BlsctPoint* get_ctx_out_ephemeral_key_c(
-    const uint8_t* ser_ctx_out,
-    const size_t ser_ctx_out_size
-);
-
-const BlsctPoint* get_ctx_out_blinding_key_c(
-    const uint8_t* ser_ctx_out,
-    const size_t ser_ctx_out_size
-);
-
-const BlsctRetVal* get_ctx_out_range_proof_c(
-    const uint8_t* ser_ctx_out,
-    const size_t ser_ctx_out_size
-);
-
-uint16_t get_ctx_out_view_tag_c(
-    const uint8_t* ser_ctx_out,
-    const size_t ser_ctx_out_size
-);
-
-const BlsctTokenId* get_ctx_out_token_id_c(
-    const uint8_t* ser_ctx_out,
-    const size_t ser_ctx_out_size
-);
-
-const BlsctRetVal* get_ctx_out_vector_predicate_c(
-    const uint8_t* ser_ctx_out,
-    const size_t ser_ctx_out_size
-);
 
 // tx
 
