f7de979972059fa58620a3d1a8c64759d2f46ab9
alex v <alex@encrypt-s.com>
2025-11-22 14:17:49 +0700

    Disable linter for thread safety

--- src/node/coin.cpp
+++ src/node/coin.cpp
@@ -17,6 +17,8 @@ void FindCoins(const NodeContext& node, std::map<COutPoint, Coin>& coins)
     CCoinsViewCache& chain_view = node.chainman->ActiveChainstate().CoinsTip();
     CCoinsViewMemPool mempool_view(&chain_view, *node.mempool);
     for (auto& coin : coins) {
+        // mempool_view.mempool is the same as *node.mempool, so mempool_view.mempool.cs is already held
+        // NOLINTNEXTLINE(thread-safety-precise)
         if (!mempool_view.GetCoin(coin.first, coin.second)) {
             // Either the coin is not in the CCoinsViewCache or is spent. Clear it.
             coin.second.Clear();
