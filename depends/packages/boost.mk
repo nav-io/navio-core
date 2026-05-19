1898e290e8a67d277ad76b401168817670d04fad
mxaddict <mxaddict@kryptic.sh>
2026-05-19 18:23:30 +0800

    depends: bump boost to 1.84.0 and drop process_macos_sdk patch
    The upstream sync (37429bd74e) deleted the patch file but left the
    reference, breaking all boost builds. Rather than restore an
    already-obsolete patch, bump boost to 1.84.0 where the Boost::Process
    macOS-SDK issue is fixed upstream (per the patch's own comment).
    
    Full upstream parity (1.90 + CMake buildsystem + drop boost::process
    for util/subprocess) is a separate, larger migration tracked in a
    follow-up issue.

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,14 +1,9 @@
 package=boost
-$(package)_version=1.81.0
+$(package)_version=1.84.0
 # JFrog CDN is retired; Boost hosts releases on archives.boost.io.
 $(package)_download_path=https://archives.boost.io/release/$($(package)_version)/source/
 $(package)_file_name=boost_$(subst .,_,$($(package)_version)).tar.bz2
-$(package)_sha256_hash=71feeed900fbccca04a3b4f2f84a7c217186f28a940ed8b7ed4725986baf99fa
-$(package)_patches=process_macos_sdk.patch
-
-define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/process_macos_sdk.patch
-endef
+$(package)_sha256_hash=cc4b893acf645c9d4b698e9a0f08ca8846aa5d6c68275c14c3e7949c24109454
 
 define $(package)_stage_cmds
   mkdir -p $($(package)_staging_prefix_dir)/include && \
